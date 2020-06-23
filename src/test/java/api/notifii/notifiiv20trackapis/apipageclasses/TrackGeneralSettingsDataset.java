8254e44971a0a8e5434edb876e901db1ba80c818
santhosh-annam <santhosh.annam@devrabbit.com>
2020-06-23 18:43:20 +0530

    push2

---
+++ src/test/java/api/notifii/notifiiv20trackapis/apipageclasses/TrackGeneralSettingsDataset.java
@@ -0,0 +1,96 @@
+package api.notifii.notifiiv20trackapis.apipageclasses; 
+
+public class TrackGeneralSettingsDataset{
+
+public static final int datasetLength = 2; 
+
+public static String projectName = "notifii";
+
+			/*--------Dataset#1 Dataset-----------*/
+
+public static final String datasetHeader1 = "TC_GeneralSettings-Dataset#1";
+
+public static final String datasetResources1 = "track/getTrackSettings";
+public static final int requestType1 = 1;
+public static final int bodyType1 = 0;
+public static final String apiName1 = "Track General Settings";
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[{\"header_id\":958,"
+ + "\"header_value\":\"U2FsdGVkX1/WDbA5PWhe0BiALDOioEvlPorQYX5Of1w=\","
+ + "\"dataset_id\":1625,"
+ + "\"header_key\":\"userId\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":959,"
+ + "\"header_value\":\"U2FsdGVkX18YFB2Xap+A/pRRSs+SWotsKw8ODGo/kYg=\","
+ + "\"dataset_id\":1625,"
+ + "\"header_key\":\"accountId\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":960,"
+ + "\"header_value\":\"U2FsdGVkX18PlrX525cJHVRj1dMDN8eIGt3b/3UJLoQ=\","
+ + "\"dataset_id\":1625,"
+ + "\"header_key\":\"sessionId\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":957,"
+ + "\"header_value\":\"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyUmVxdWVzdCI6eyJ1c2VybmFtZSI6ImRyaGFjYzEiLCJwYXNzd29yZCI6ImRyaGFjYzEiLCJzZXNzaW9uX3RpbWVkb3V0IjoiMWQifSwiaWF0IjoxNTkyOTA3Njk2LCJleHAiOjE1OTI5OTQwOTZ9.IgXikrE7vaxBimGZODCCqrR2iVdWzgEpqY1LeeCFI8s\","
+ + "\"dataset_id\":1625,"
+ + "\"header_key\":\"authenticationToken\","
+ + "\"header_desc\":\"\"}]";
+
+public static final String authenticationData1 = "{\"authtype\":\"noauth\","
+ + "\"basicauth\":{\"username\":\"\","
+ + "\"password\":\"\"},"
+ + "\"apikey\":{\"key\":\"\","
+ + "\"value\":\"\","
+ + "\"addto\":\"header\"},"
+ + "\"bearertoken\":{\"token\":\"\"}}";
+
+
+ public static String body1  = "{}";
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader2 = "TC_GeneralSettings-Default";
+
+public static final String datasetResources2 = "track/getTrackSettings";
+public static final int requestType2 = 1;
+public static final int bodyType2 = 0;
+public static final String apiName2 = "Track General Settings";
+
+ public static String urlParams2  = "[]";
+
+ public static String headers2  = "[{\"header_id\":961,"
+ + "\"header_value\":\"Bearer $#AUTHENTICATIONTOKEN#$\","
+ + "\"dataset_id\":1623,"
+ + "\"header_key\":\"authenticationToken\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":962,"
+ + "\"header_value\":\"$#USERID#$\","
+ + "\"dataset_id\":1623,"
+ + "\"header_key\":\"userId\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":963,"
+ + "\"header_value\":\"$#ACCOUNTID#$\","
+ + "\"dataset_id\":1623,"
+ + "\"header_key\":\"accountId\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":964,"
+ + "\"header_value\":\"$#SESSIONID#$\","
+ + "\"dataset_id\":1623,"
+ + "\"header_key\":\"sessionId\","
+ + "\"header_desc\":\"\"}]";
+
+public static final String authenticationData2 = "{\"authtype\":\"noauth\","
+ + "\"basicauth\":{\"username\":\"\","
+ + "\"password\":\"\"},"
+ + "\"apikey\":{\"key\":\"\","
+ + "\"value\":\"\","
+ + "\"addto\":\"header\"},"
+ + "\"bearertoken\":{\"token\":\"\"}}";
+
+
+ public static String body2  = "{}";
+
+
+}
