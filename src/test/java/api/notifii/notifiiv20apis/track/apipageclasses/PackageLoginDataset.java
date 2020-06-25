d83dc6983fbafaf93155af966295732716f21d01
santhosh-annam <santhosh.annam@devrabbit.com>
2020-06-25 15:59:32 +0530

    latest execution

--- src/test/java/api/notifii/notifiiv20apis/track/apipageclasses/PackageLoginDataset.java
+++ src/test/java/api/notifii/notifiiv20apis/track/apipageclasses/PackageLoginDataset.java
@@ -6,9 +6,9 @@ public static final int datasetLength = 1;
 
 public static String projectName = "notifii";
 
-			/*--------Dataset# Valid Details Dataset-----------*/
+			/*--------Dataset# Without RecipientID Dataset-----------*/
 
-public static final String datasetHeader1 = "TC_PackageLogIN-Dataset# Valid Details";
+public static final String datasetHeader1 = "TC_PackageLogIN-Dataset# Without RecipientID";
 
 public static final String datasetResources1 = "track/packageLogin";
 public static final int requestType1 = 2;
@@ -17,24 +17,24 @@ public static final String apiName1 = "Package Login";
 
  public static String urlParams1  = "[]";
 
- public static String headers1  = "[{\"header_id\":1473,"
+ public static String headers1  = "[{\"header_id\":1390,"
  + "\"header_value\":\"Bearer $#AUTHENTICATIONTOKEN#$\","
- + "\"dataset_id\":1658,"
+ + "\"dataset_id\":1659,"
  + "\"header_key\":\"authenticationToken\","
  + "\"header_desc\":\"\"},"
- + "{\"header_id\":1474,"
+ + "{\"header_id\":1392,"
  + "\"header_value\":\"$#USERID#$\","
- + "\"dataset_id\":1658,"
+ + "\"dataset_id\":1659,"
  + "\"header_key\":\"userId\","
  + "\"header_desc\":\"\"},"
- + "{\"header_id\":1475,"
+ + "{\"header_id\":1389,"
  + "\"header_value\":\"$#ACCOUNTID#$\","
- + "\"dataset_id\":1658,"
+ + "\"dataset_id\":1659,"
  + "\"header_key\":\"accountId\","
  + "\"header_desc\":\"\"},"
- + "{\"header_id\":1476,"
+ + "{\"header_id\":1391,"
  + "\"header_value\":\"$#SESSIONID#$\","
- + "\"dataset_id\":1658,"
+ + "\"dataset_id\":1659,"
  + "\"header_key\":\"sessionId\","
  + "\"header_desc\":\"\"}]";
 
@@ -47,25 +47,25 @@ public static final String authenticationData1 = "{\"authtype\":\"noauth\","
  + "\"bearertoken\":{\"token\":\"\"}}";
 
 
- public static String body1  = "{\"raw_text\":\"{\\n\\t\\\"trackingNumber\\\": [\\\"1234567895\\\"],"
+ public static String body1  = "{\"raw_text\":\"{\\n\\t\\\"trackingNumber\\\": [123456789],"
  + "\\n\\t\\\"shippingCarrier\\\":\\\"Amazon\\\","
- + "\\n\\t\\\"recipientId\\\": \\\"3716669\\\","
+ + "\\n\\t\\\"recipientId\\\": \\\"\\\","
  + "\\n\\t\\\"handlingUrgent\\\": 1,"
  + "\\n\\t\\\"handlingfragile\\\": 1,"
  + "\\n\\t\\\"handlingPerishable\\\": 1,"
  + "\\n\\t\\\"handlingheavy\\\": 1,"
- + "\\n\\t\\\"sender\\\": \\\"Mi\\\","
- + "\\n\\t\\\"serviceType\\\": \\\"First Class\\\","
- + "\\n\\t\\\"packagecondition\\\": \\\"Appears Fine\\\","
+ + "\\n\\t\\\"sender\\\": \\\"\\\","
+ + "\\n\\t\\\"serviceType\\\": \\\"\\\","
+ + "\\n\\t\\\"packagecondition\\\": \\\"\\\","
  + "\\n\\t\\\"weight\\\": \\\"\\\","
  + "\\n\\t\\\"dimensions\\\": \\\"\\\","
- + "\\n\\t\\\"packageType\\\": \\\"Box\\\","
- + "\\n\\t\\\"customMessage\\\": \\\"Pick up the package ASAP\\\","
- + "\\n\\t\\\"shelf\\\": \\\"Top Shelf\\\","
+ + "\\n\\t\\\"packageType\\\": \\\"\\\","
+ + "\\n\\t\\\"customMessage\\\": \\\"\\\","
+ + "\\n\\t\\\"shelf\\\": \\\"\\\","
  + "\\n\\t\\\"tagNumber\\\": 1,"
  + "\\n\\t\\\"ocrTriggered\\\": 1\\n}\","
- + "\"raw_id\":1409,"
- + "\"dataset_id\":1658,"
+ + "\"raw_id\":1391,"
+ + "\"dataset_id\":1659,"
  + "\"raw_type_id\":3}";
 
 }
