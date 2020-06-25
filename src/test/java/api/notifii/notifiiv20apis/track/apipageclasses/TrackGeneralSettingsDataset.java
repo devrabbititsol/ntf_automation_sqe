5c4a1ef846aee8b8a59a9e9ce1fae8380ec5821e
santhosh-annam <santhosh.annam@devrabbit.com>
2020-06-25 14:29:02 +0530

    latest execution

--- src/test/java/api/notifii/notifiiv20apis/track/apipageclasses/TrackGeneralSettingsDataset.java
+++ src/test/java/api/notifii/notifiiv20apis/track/apipageclasses/TrackGeneralSettingsDataset.java
@@ -6,36 +6,36 @@ public static final int datasetLength = 1;
 
 public static String projectName = "notifii";
 
-			/*--------Dataset #Invalid Authentication Dataset-----------*/
+			/*--------Dataset# Valid Details Dataset-----------*/
 
-public static final String datasetHeader1 = "TC_GetGeneralSettingsAPI-Dataset #Invalid Authentication";
+public static final String datasetHeader1 = "TC_GetGeneralSettingsAPI-Dataset# Valid Details";
 
 public static final String datasetResources1 = "track/getTrackSettings";
 public static final int requestType1 = 1;
-public static final int bodyType1 = 3;
+public static final int bodyType1 = 0;
 public static final String apiName1 = "Track General Settings";
 
  public static String urlParams1  = "[]";
 
- public static String headers1  = "[{\"header_id\":1057,"
- + "\"header_value\":\"$#ACCOUNTID#$\","
- + "\"dataset_id\":1633,"
- + "\"header_key\":\"accountId\","
- + "\"header_desc\":\"\"},"
- + "{\"header_id\":1058,"
+ public static String headers1  = "[{\"header_id\":1062,"
  + "\"header_value\":\"$#SESSIONID#$\","
- + "\"dataset_id\":1633,"
+ + "\"dataset_id\":1632,"
  + "\"header_key\":\"sessionId\","
  + "\"header_desc\":\"\"},"
- + "{\"header_id\":1059,"
- + "\"header_value\":\"$#AUTHENTICATIONTOKEN#$\","
- + "\"dataset_id\":1633,"
+ + "{\"header_id\":1063,"
+ + "\"header_value\":\"Bearer $#AUTHENTICATIONTOKEN#$\","
+ + "\"dataset_id\":1632,"
  + "\"header_key\":\"authenticationToken\","
  + "\"header_desc\":\"\"},"
- + "{\"header_id\":1060,"
+ + "{\"header_id\":1064,"
  + "\"header_value\":\"$#USERID#$\","
- + "\"dataset_id\":1633,"
+ + "\"dataset_id\":1632,"
  + "\"header_key\":\"userId\","
+ + "\"header_desc\":\"\"},"
+ + "{\"header_id\":1061,"
+ + "\"header_value\":\"$#ACCOUNTID#$\","
+ + "\"dataset_id\":1632,"
+ + "\"header_key\":\"accountId\","
  + "\"header_desc\":\"\"}]";
 
 public static final String authenticationData1 = "{\"authtype\":\"noauth\","
